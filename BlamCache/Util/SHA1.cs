56463d1ac9b93def6bf98c2809be9d1bf34fe3e3
amd7 <amd@xboxchaos.com>
2013-01-12 14:52:25 -0600

    Really basic SHA1 hashing

---
+++ BlamCache/Util/SHA1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ExtryzeDLL.Util
+{
+    public static class SHA1
+    {
+        public static byte[] Transform(byte[] data)
+        {
+            return _sha1.ComputeHash(data);
+        }
+
+        private static SHA1CryptoServiceProvider _sha1 = new SHA1CryptoServiceProvider();
+    }
+}
