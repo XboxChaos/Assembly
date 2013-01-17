2ad742519e3337e791840bcb06fc12d98f81a9aa
Xerax <alexreed@outlook.com>
2013-01-17 00:26:36 +0000

    Cleanup of 'Assembly' namespace

--- Assembly/App.xaml.cs
+++ Assembly/App.xaml.cs
@@ -1,51 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.IO;
-using System.Linq;
 using System.Windows;
-using System.Xml;
 using Assembly.Helpers;
-using Assembly.Helpers.Plugins;
-using Assembly.Metro.Dialogs;
 using XBDMCommunicator;
 using Microsoft.Shell;
-using Assembly.Windows;
-using Assembly.Helpers.Cryptography;
 
 namespace Assembly
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application, ISingleInstanceApp
+    public partial class App : ISingleInstanceApp
     {
         #region ISingleInstanceApp Members
         public bool SignalExternalCommandLineArgs(IList<string> args)
         {
-            if (Settings.homeWindow != null)
-                return Settings.homeWindow.ProcessCommandLineArgs(args);
-            else 
-                return true;
+	        return Settings.homeWindow == null || Settings.homeWindow.ProcessCommandLineArgs(args);
         }
 
-        
-        #endregion
+	    #endregion
 
         [STAThread]
         public static void Main()
         {
-            if (SingleInstance<App>.InitializeAsFirstInstance("RecivedCommand"))
-            {
-                var application = new App();
+	        if (!SingleInstance<App>.InitializeAsFirstInstance("RecivedCommand")) return;
 
-                application.InitializeComponent();
-                application.Run();
+	        var application = new App();
 
-                // Allow single instance code to perform cleanup operations
-                SingleInstance<App>.Cleanup();
-            }
+	        application.InitializeComponent();
+	        application.Run();
+
+	        // Allow single instance code to perform cleanup operations
+	        SingleInstance<App>.Cleanup();
         }
 
         protected override void OnStartup(StartupEventArgs e)
@@ -79,7 +65,7 @@ namespace Assembly
             FileDefaults.UpdateFileDefaults();
 
             // Set closing method
-            Application.Current.Exit += (o, args) =>
+            Current.Exit += (o, args) =>
                 {
                     // Update Settings with Window Width/Height
                     Settings.applicationSizeMaximize = (Settings.homeWindow.WindowState == WindowState.Maximized);
@@ -101,59 +87,6 @@ namespace Assembly
                     args.Handled = true;
                 };
 #endif
-
-            #region Hide Shitty Code
-            // Check if Startup Params Exist
-            // ERHMAGAWD - So much haxxy stuff >.>
-//            if (e.Args != null && e.Args.Length > 0)
-//            {
-//                string[] args = new string[0];
-//                if (e.Args[0].ToLower().Contains("assembly://"))
-//                {
-//                    e.Args[0] = e.Args[0].Replace("assembly://", "");
-//                    args = e.Args[0].Split('/');
-//                }
-//                else
-//                {
-//                    if (File.Exists(e.Args[0]))
-//                    {
-//                        FileInfo fi = new FileInfo(e.Args[0]);
-//                        args = new string[2];
-//                        args[1] = fi.FullName;
-
-//                        if (fi.Extension.ToLower() == ".map")
-//                            args[0] = "map";
-//                        else if (fi.Extension.ToLower() == ".blf")
-//                            args[0] = "blf";
-//                        else if (fi.Extension.ToLower() == ".mapinfo")
-//                            args[0] = "mapinfo";
-//                        else
-//                            args = new string[2] { "", "" };
-//                    }
-//                    else
-//                        args = new string[2] { "", "" };
-//                }
-//#if DEBUG
-//                try { File.WriteAllLines(@"C:/args.txt", args); } catch { }
-//#endif
-//                appStartupManager = new StartupManager(args);
-//            }
-            #endregion
-        }
-
-        public class Validate
-        {
-            public string action = "valUsr";
-            public Inner Content;
-
-            
-            public class Inner
-            {
-                public string machName { get; set; }
-                public string machNameSec { get; set; }
-                public string compName { get; set; }
-                public string userName { get; set; }
-            }
         }
     }
 }
