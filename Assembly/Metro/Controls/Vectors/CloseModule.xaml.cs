abf7768ee210216fa41d8f34042ab8e4660eb3b7
Xerax <alexreed@outlook.com>
2013-01-17 01:21:02 +0000

    Cleanup of 'Controls' namespace started.
    I'll do more when I feel like it, it's soooooooooo boring.

--- Assembly/Metro/Controls/Vectors/CloseModule.xaml.cs
+++ Assembly/Metro/Controls/Vectors/CloseModule.xaml.cs
@@ -1,26 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Assembly.Metro.Controls.Vectors
+﻿namespace Assembly.Metro.Controls.Vectors
 {
 	/// <summary>
 	/// Interaction logic for CloseModule.xaml
 	/// </summary>
-	public partial class CloseModule : UserControl
+	public partial class CloseModule
 	{
 		public CloseModule()
 		{
-			this.InitializeComponent();
+			InitializeComponent();
 		}
 	}
 }
