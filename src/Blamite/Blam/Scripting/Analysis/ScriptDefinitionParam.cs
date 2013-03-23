83e072322573f4e083f735852c7de05f72568b5e
amd7 <amd@xboxchaos.com>
2013-03-23 10:29:09 -0500

    Added node classes for script parsing/AST building

---
+++ src/Blamite/Blam/Scripting/Analysis/ScriptDefinitionParam.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blamite.Blam.Scripting.Analysis
+{
+    /// <summary>
+    /// A parameter in a script definition.
+    /// </summary>
+    public class ScriptDefinitionParam
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScriptDefinitionParam"/> class.
+        /// </summary>
+        /// <param name="name">The name of the parameter.</param>
+        /// <param name="type">The type of the parameter.</param>
+        public ScriptDefinitionParam(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        /// <summary>
+        /// Gets the name of the parameter.
+        /// </summary>
+        /// <value>The name of the parameter.</value>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets the type of the parameter.
+        /// </summary>
+        /// <value>The type of the parameter.</value>
+        public string Type { get; private set; }
+    }
+}
