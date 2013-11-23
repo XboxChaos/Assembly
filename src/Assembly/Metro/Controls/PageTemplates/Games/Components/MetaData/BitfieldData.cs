ae1c50c1240ca32341617a46016766cb191d6609
amd7 <amd@xboxchaos.com>
2013-11-23 13:30:22 -0500

    Resolves #122

--- src/Assembly/Metro/Controls/PageTemplates/Games/Components/MetaData/BitfieldData.cs
+++ src/Assembly/Metro/Controls/PageTemplates/Games/Components/MetaData/BitfieldData.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace Assembly.Metro.Controls.PageTemplates.Games.Components.MetaData
 {
@@ -24,6 +25,8 @@ namespace Assembly.Metro.Controls.PageTemplates.Games.Components.MetaData
             : base(name, offset, address, pluginLine)
         {
             _type = type;
+            CheckAllCommand = new QuickCheckCommand(this, true);
+            UncheckAllCommand = new QuickCheckCommand(this, false);
         }
 
         public void DefineBit(int index, string name)
@@ -59,6 +62,10 @@ namespace Assembly.Metro.Controls.PageTemplates.Games.Components.MetaData
             get { return _bits.Values; }
         }
 
+        public ICommand CheckAllCommand { get; private set; }
+
+        public ICommand UncheckAllCommand { get; private set; }
+
         public override void Accept(IMetaFieldVisitor visitor)
         {
             visitor.VisitBitfield(this);
@@ -77,6 +84,58 @@ namespace Assembly.Metro.Controls.PageTemplates.Games.Components.MetaData
         {
             foreach (BitData bit in Bits)
                 bit.Refresh();
+        }
+
+        /// <summary>
+        /// Command that quickly checks or unchecks all bits in a bitfield.
+        /// </summary>
+        private class QuickCheckCommand : ICommand
+        {
+            private BitfieldData _data;
+            private bool _check;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="QuickCheckCommand"/> class.
+            /// </summary>
+            /// <param name="data">The biefield.</param>
+            /// <param name="check">If set to <c>true</c>, the command will check all bits in the field, otherwise it will uncheck all bits in the field.</param>
+            public QuickCheckCommand(BitfieldData data, bool check)
+            {
+                _data = data;
+                _check = check;
+            }
+
+            /// <summary>
+            /// Defines the method that determines whether the command can execute in its current state.
+            /// </summary>
+            /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+            /// <returns>
+            /// true if this command can be executed; otherwise, false.
+            /// </returns>
+            public bool CanExecute(object parameter)
+            {
+                // Just make sure the bitfield has bits in it
+                return _data.Bits.Any();
+            }
+
+            /// <summary>
+            /// Occurs when changes occur that affect whether or not the command should execute.
+            /// </summary>
+            public event EventHandler CanExecuteChanged
+            {
+                add { }
+                remove { }
+            }
+
+            /// <summary>
+            /// Defines the method to be called when the command is invoked.
+            /// </summary>
+            /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+            public void Execute(object parameter)
+            {
+                foreach (var bit in _data.Bits)
+                    bit.IsSet = _check;
+            }
         }
     }
 
