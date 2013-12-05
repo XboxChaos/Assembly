7c5c148180e6e5df7809d67b227a9a348d3596dc
Alex Reed (Xerax) <alexreed@outlook.com>
2013-12-05 06:59:25 +0000

    Improved/corrected/fixed my code, based on my improved understanding of the Wavefront Object file format.

--- src/Assembly/Helpers/Models/ObjImporter.cs
+++ src/Assembly/Helpers/Models/ObjImporter.cs
@@ -19,23 +19,46 @@ namespace Assembly.Helpers.Models
 			_input = new StreamReader(File.Open(inPath, FileMode.Open, FileAccess.Read));
 		}
 
-		public class Temp
+		public class WaveFrontObject
 		{
-			public List<ushort> Indices { get; set; }
+			public IList<Face> FaceDefinitions { get; set; }
 
 			public int BaseIndex { get; set; }
 
 			public string Name { get; set; }
+
+			public class Vertex
+			{
+				public float X { get; set; }
+				public float Y { get; set; }
+				public float Z { get; set; }
+			}
+
+			public class Face
+			{
+				public IList<Element> Elements { get; set; } 
+
+				public class Element
+				{
+					public Vertex Vertex { get; set; }
+					public Vertex VertexTextureCoordinate { get; set; }
+					public Vertex VertexNormal { get; set; }
+				}
+			}
 		}
 
 		public void Process()
 		{
-			var stuff = new Temp
+			var wavefrontObject = new WaveFrontObject
 			{
-				Indices = new List<ushort>(),
+				FaceDefinitions = new List<WaveFrontObject.Face>(),
 				Name = "",
 				BaseIndex = 0
 			};
+
+			var vertices = new List<WaveFrontObject.Vertex>();
+			var vertexNormals = new List<WaveFrontObject.Vertex>();
+			var vertexTextureCoordinates = new List<WaveFrontObject.Vertex>();
 
 			while (!_input.EndOfStream)
 			{
@@ -44,17 +67,47 @@ namespace Assembly.Helpers.Models
 					break;
 				line = line.ToLower();
 
-				if (line.StartsWith("# baseIndex = "))
-					stuff.BaseIndex = int.Parse(line.Replace("# baseIndex = ", ""));
+				if (line.StartsWith("v "))
+				{
+					var coords = line.Remove(0, 2).Trim().Split(' ');
+					var vertex = new WaveFrontObject.Vertex
+					{
+						X = float.Parse(coords[0]),
+						Y = float.Parse(coords[1]),
+						Z = float.Parse(coords[2]),
+					};
+
+					if (line.StartsWith("vn"))
+						vertices.Add(vertex);
+					else if (line.StartsWith("vt"))
+						vertices.Add(vertex);
+					else if (line.StartsWith("v"))
+						vertices.Add(vertex);
+				}
+				else if (line.StartsWith("# baseindex = "))
+					wavefrontObject.BaseIndex = int.Parse(line.Replace("# baseindex = ", ""));
 				else if (line.StartsWith("g "))
-					stuff.Name = line.Replace("g ", "");
+					wavefrontObject.Name = line.Replace("g", "").Trim();
 				else if (line.StartsWith("f "))
 				{
-					var match = Regex.Match(line, @"f[ ]+([0-9\/])+ ([0-9\/])+ ([0-9\/])+", RegexOptions.IgnoreCase);
-					for(var i = 1; i < 4; i++)
-						stuff.Indices.Add(ushort.Parse(match.Groups[i].Captures[0].Value));
-				}
+					var match = Regex.Match(line, @"f[ ]+([0-9\/]+) ([0-9\/]+) ([0-9\/]+)", RegexOptions.IgnoreCase);
+					var face = new WaveFrontObject.Face { Elements = new List<WaveFrontObject.Face.Element>() };
+					for (var i = 1; i < 4; i++)
+					{
+						var section = match.Groups[i].Captures[0].Value.Split('/');
+						var vertexIndex = int.Parse(section[0]) - 1;
+						var vertexTextureCoordinateIndex = int.Parse(section[1]) - 1;
+						var vertexNormalIndex = int.Parse(section[2]) - 1;
 
+						face.Elements.Add(new WaveFrontObject.Face.Element
+						{
+							Vertex = vertices[vertexIndex],
+							VertexTextureCoordinate = vertexTextureCoordinates[vertexTextureCoordinateIndex],
+							VertexNormal = vertexNormals[vertexNormalIndex],
+						});
+					}
+					wavefrontObject.FaceDefinitions.Add(face);
+				}
 
 				Debug.WriteLine(line);
 			}
