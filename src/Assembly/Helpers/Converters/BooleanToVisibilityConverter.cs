67e795b6933228666cf890f2500d4b4c236bc552
Alex Reed (Xerax) <alexreed@outlook.com>
2013-12-16 05:58:14 +0000

    updated the block data component.

---
+++ src/Assembly/Helpers/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Assembly.Helpers.Converters
+{
+	public class BooleanToVisibilityConverter : IValueConverter
+	{
+		public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+		{
+			if (targetType != typeof (Visibility))
+				throw new InvalidOperationException("Converter can only convert to value of type Visibility.");
+
+			var visible = System.Convert.ToBoolean(value, culture);
+			if (InvertVisibility) visible = !visible;
+			return visible ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+		{
+			return value;
+		}
+
+		public Boolean InvertVisibility { get; set; }
+	}
+}
