7d795dfaa2e77e9244ccd23961d3d7e9c2387b4f
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-01 16:53:21 +0000

    more work on new ui. but window dragging has broken, shoulld probally fix that.

--- src/Atlas/Metro/Controls/Custom/MetroWindow.cs
+++ src/Atlas/Metro/Controls/Custom/MetroWindow.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls.Primitives;
@@ -9,136 +7,24 @@ using Atlas.Native;
 
 namespace Atlas.Metro.Controls.Custom
 {
-	public class MetroWindow : Window, INotifyPropertyChanged
+	public class MetroWindow : Window
 	{
-		public static DependencyProperty WindowStatusProperty;
-		public static DependencyProperty WindowTitleProperty;
-		public static DependencyProperty WindowMaskVisibilityProperty;
-
-		public static DependencyProperty WindowBorderThicknessProperty;
-		public static DependencyProperty WindowActionRestoreVisibilityProperty;
-		public static DependencyProperty WindowActionMaximizeVisibilityProperty;
-
-		public static DependencyProperty WindowResizingVisibilityProperty;
-
 		public MetroWindow()
 		{
-			StateChanged += OnStateChanged;
 			SourceInitialized += OnSourceInitialized;
-
-			// Update the current state, for old times sake
-// ReSharper disable once DoNotCallOverridableMethodsInConstructor
-			OnStateChanged(null);
 		}
-
-		static MetroWindow()
-		{
-			WindowStatusProperty = 
-				DependencyProperty.Register("WindowStatus", typeof(String), typeof(MetroWindow));
-			WindowTitleProperty = 
-				DependencyProperty.Register("WindowTitle", typeof(String), typeof(MetroWindow));
-			WindowMaskVisibilityProperty = 
-				DependencyProperty.Register("WindowMaskVisibility", typeof(Boolean), typeof(MetroWindow));
-
-			WindowBorderThicknessProperty =
-				DependencyProperty.Register("WindowBorderThickness", typeof(Thickness), typeof(MetroWindow));
-			WindowActionRestoreVisibilityProperty =
-				DependencyProperty.Register("WindowActionRestoreVisibility", typeof(Visibility), typeof(MetroWindow));
-			WindowActionMaximizeVisibilityProperty =
-				DependencyProperty.Register("WindowActionMaximizeVisibility", typeof(Visibility), typeof(MetroWindow));
-
-			WindowResizingVisibilityProperty =
-				DependencyProperty.Register("WindowResizingVisibility", typeof(Visibility), typeof(MetroWindow));
-		}
-
-		#region Properties
-
-		#region Window
-
-		public String WindowStatus
-		{
-			get { return (String)GetValue(WindowStatusProperty); }
-			set { SetField(WindowStatusProperty, value); }
-		}
-
-		public String WindowTitle
-		{
-			get { return (String)GetValue(WindowTitleProperty); }
-			set { SetField(WindowTitleProperty, value); }
-		}
-
-		public Boolean WindowMaskVisibility
-		{
-			get { return (Boolean)GetValue(WindowMaskVisibilityProperty); }
-			set { SetField(WindowMaskVisibilityProperty, value); }
-		}
-
-		#endregion
-
-		#region Window Actions
-
-		public Thickness WindowBorderThickness
-		{
-			get { return (Thickness)GetValue(WindowBorderThicknessProperty); }
-			set { SetField(WindowBorderThicknessProperty, value); }
-		}
-
-		public Visibility WindowActionRestoreVisibility
-		{
-			get { return (Visibility)GetValue(WindowActionRestoreVisibilityProperty); }
-			set { SetField(WindowActionRestoreVisibilityProperty, value); }
-		}
-
-		public Visibility WindowActionMaximizeVisibility
-		{
-			get { return (Visibility)GetValue(WindowActionMaximizeVisibilityProperty); }
-			set { SetField(WindowActionMaximizeVisibilityProperty, value); }
-		}
-
-		#endregion
-
-		#region Resizing
-
-		public Visibility WindowResizingVisibility
-		{
-			get { return (Visibility)GetValue(WindowResizingVisibilityProperty); }
-			set { SetField(WindowResizingVisibilityProperty, value); }
-		}
-
-		#endregion
-
-		#endregion
 
 		#region Events
 
-		private void OnStateChanged(object sender, EventArgs eventArgs)
-		{
-			switch (WindowState)
-			{
-				case WindowState.Normal:
-					WindowBorderThickness = new Thickness(1, 1, 1, 25);
-					WindowActionRestoreVisibility = Visibility.Collapsed;
-					WindowActionMaximizeVisibility = Visibility.Visible;
-					WindowResizingVisibility = Visibility.Visible;
-					break;
-				case WindowState.Maximized:
-					WindowBorderThickness = new Thickness(0, 0, 0, 25);
-					WindowActionRestoreVisibility = Visibility.Visible;
-					WindowActionMaximizeVisibility = Visibility.Collapsed;
-					WindowResizingVisibility = Visibility.Collapsed;
-					break;
-			}
-		}
+		#region Maximize Workspace Workarounds
 
-		private void OnSourceInitialized(object sender, EventArgs eventArgs)
+		public void OnSourceInitialized(object sender, EventArgs eventArgs)
 		{
 			var handle = (new WindowInteropHelper(this)).Handle;
 			var hwndSource = HwndSource.FromHwnd(handle);
 			if (hwndSource != null)
 				hwndSource.AddHook(WindowProc);
 		}
-
-		#region Maximize Workspace Workarounds
 
 		private static IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
 		{
@@ -170,13 +56,6 @@ namespace Atlas.Metro.Controls.Custom
 				mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
 				mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
 				mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
-
-				/*
-				mmi.ptMaxPosition.x = Math.Abs(scrn.Bounds.Left - scrn.WorkingArea.Left);
-				mmi.ptMaxPosition.y = Math.Abs(scrn.Bounds.Top - scrn.WorkingArea.Top);
-				mmi.ptMaxSize.x = Math.Abs(scrn.Bounds.Right - scrn.WorkingArea.Left);
-				mmi.ptMaxSize.y = Math.Abs(scrn.Bounds.Bottom - scrn.WorkingArea.Top);
-				*/
 			}
 
 			Marshal.StructureToPtr(mmi, lParam, true);
@@ -238,24 +117,6 @@ namespace Atlas.Metro.Controls.Custom
 		}
 
 		#endregion
-
-		#endregion
-
-		#region Binding
-
-		public event PropertyChangedEventHandler PropertyChanged;
-		protected virtual void OnPropertyChanged(string propertyName)
-		{
-			if (PropertyChanged != null)
-				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-		}
-		protected bool SetField<T>(DependencyProperty property, T value, 
-			[CallerMemberName] string propertyName = "")
-		{
-			SetValue(property, value);
-			OnPropertyChanged(propertyName);
-			return true;
-		}
 
 		#endregion
 	}
---
+++ src/Atlas/ViewModels/HomeViewModel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using Atlas.Metro.Controls.Custom;
+
+namespace Atlas.ViewModels
+{
+	public class HomeViewModel : MetroWindow, INotifyPropertyChanged
+	{
+		#region Properties
+
+		#region Application Stuff
+
+		public string Status
+		{
+			get { return _status; }
+			set { SetField(ref _status, value); }
+		}
+		private string _status = "Ready...";
+
+		public string ApplicationTitle
+		{
+			get { return _applicationTitle; }
+			set { SetField(ref _applicationTitle, value); }
+		}
+		private string _applicationTitle = "Welcome";
+
+		public Thickness ApplicationBorderThickness
+		{
+			get { return _applicationBorderThickness; }
+			set { SetField(ref _applicationBorderThickness, value); }
+		}
+		private Thickness _applicationBorderThickness;
+
+		#endregion
+
+		#region Window Masks
+
+		public Visibility MaskVisibility
+		{
+			get { return _maskVisibility; }
+			set { SetField(ref _maskVisibility, value); }
+		}
+		private Visibility _maskVisibility = Visibility.Collapsed;
+
+		#endregion
+
+		#region Window Actions/Resizing
+
+		public Visibility ActionRestoreVisibility
+		{
+			get { return _actionRestoreVisibility; }
+			set { SetField(ref _actionRestoreVisibility, value); }
+		}
+		private Visibility _actionRestoreVisibility = Visibility.Collapsed;
+
+		public Visibility ActionMaximizeVisibility
+		{
+			get { return _actionMaximizeVisibility; }
+			set { SetField(ref _actionMaximizeVisibility, value); }
+		}
+		private Visibility _actionMaximizeVisibility = Visibility.Collapsed;
+
+		public Visibility ResizingVisibility
+		{
+			get { return _resizingVisibility; }
+			set { SetField(ref _resizingVisibility, value); }
+		}
+		private Visibility _resizingVisibility = Visibility.Collapsed;
+
+		#endregion
+
+		#endregion
+
+		#region Events
+
+		#region Overrides
+		public void OnStateChanged(object sender, EventArgs eventArgs)
+		{
+			switch ((WindowState)sender)
+			{
+				case WindowState.Normal:
+					ApplicationBorderThickness = new Thickness(1, 1, 1, 25);
+					ActionRestoreVisibility = Visibility.Collapsed;
+					ActionMaximizeVisibility = 
+						ResizingVisibility = Visibility.Visible;
+					break;
+				case WindowState.Maximized:
+					ApplicationBorderThickness = new Thickness(0, 0, 0, 25);
+					ActionRestoreVisibility = Visibility.Visible;
+					ActionMaximizeVisibility =
+						ResizingVisibility = Visibility.Collapsed;
+					break;
+			}
+		}
+		#endregion
+
+		#endregion
+
+		#region Binding
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+		protected bool SetField<T>(ref T field, T value,
+			[CallerMemberName] string propertyName = "")
+		{
+			if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+			field = value;
+			OnPropertyChanged(propertyName);
+			return true;
+		}
+
+		#endregion
+	}
+}
