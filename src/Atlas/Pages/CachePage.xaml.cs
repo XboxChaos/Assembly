48014f6fed6793f5b8664d04771c3ede233ffb9a
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-02 17:56:10 +0000

    fixes and additions.

---
+++ src/Atlas/Pages/CachePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Atlas.Pages
+{
+	/// <summary>
+	/// Interaction logic for CachePage.xaml
+	/// </summary>
+	public partial class CachePage : IAssemblyPage
+	{
+		public CachePage()
+		{
+			InitializeComponent();
+		}
+
+		public bool Close()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
