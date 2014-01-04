68486824489534ff6a7e884f8dc3ffd8feb528a5
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-04 15:47:04 +0000

    tag explorer search box, and treeview impovements

---
+++ src/Atlas/Metro/Controls/Custom/MetroWatermarkTextbox.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Atlas.Metro.Controls.Custom
+{
+	public class MetroWatermarkTextbox : TextBox
+	{
+		public static DependencyProperty WatermarkProperty;
+		public static DependencyProperty WatermarkForegroundProperty;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		static MetroWatermarkTextbox()
+		{
+			WatermarkProperty = DependencyProperty.Register("Watermark", typeof(String), typeof(MetroWatermarkTextbox));
+			WatermarkForegroundProperty = DependencyProperty.Register("WatermarkForeground", typeof(SolidColorBrush), typeof(MetroWatermarkTextbox));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public MetroWatermarkTextbox()
+		{
+			DefaultStyleKey = typeof(MetroWatermarkTextbox);
+			TextChanged += TextBoxTextChanged;
+			GotFocus += OnGotFocus;
+			LostFocus += OnLostFocus;
+		}
+
+		#region Region
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public String Watermark
+		{
+			get { return (String)GetValue(WatermarkProperty); }
+			set { SetValue(WatermarkProperty, value); }
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public SolidColorBrush WatermarkForeground
+		{
+			get { return (SolidColorBrush)GetValue(WatermarkForegroundProperty); }
+			set { SetValue(WatermarkForegroundProperty, value); }
+		}
+
+		#endregion
+
+		#region Base Class Overrides
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			//we need to set the initial state of the watermark
+			GoToWatermarkVisualState();
+		}
+
+		#endregion
+
+		#region Event Handlers
+
+		private void TextBoxTextChanged(object sender, TextChangedEventArgs e)
+		{
+			GoToWatermarkVisualState();
+		}
+
+		private void OnGotFocus(object sender, RoutedEventArgs routedEventArgs)
+		{
+			GoToWatermarkVisualState();
+		}
+
+		private void OnLostFocus(object sender, RoutedEventArgs routedEventArgs)
+		{
+			GoToWatermarkVisualState();
+		}
+
+		#endregion
+
+		#region Methods
+
+		private void GoToWatermarkVisualState()
+		{
+			var hasFocus = IsFocused;
+			//if our text is empty and our control doesn't have focus then show the watermark
+			//otherwise the control eirther has text or has focus which in either case we need to hide the watermark
+			if (String.IsNullOrEmpty(Text) && !hasFocus)
+				GoToVisualState("WatermarkVisible");
+			else
+				GoToVisualState("WatermarkCollapsed");
+		}
+
+		private void GoToVisualState(string stateName, bool useTransitions = true)
+		{
+			VisualStateManager.GoToState(this, stateName, useTransitions);
+		}
+
+		#endregion
+	}
+}
