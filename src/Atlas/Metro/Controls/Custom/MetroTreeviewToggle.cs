3416f82502fe14c49d32ebc8d4a99a21b42fbdf7
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-04 15:47:04 +0000

    tag explorer search box, and treeview impovements

---
+++ src/Atlas/Metro/Controls/Custom/MetroTreeviewToggle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Atlas.Metro.Controls.Custom
+{
+	public class MetroTreeViewToggle : ToggleButton
+	{
+		public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register(
+			"IsActive", typeof(Boolean), typeof(MetroTreeViewToggle), new PropertyMetadata(default(Boolean)));
+
+		public Boolean IsActive
+		{
+			get { return (Boolean)GetValue(IsActiveProperty); }
+			set { SetValue(IsActiveProperty, value); }
+		}
+	}
+}
