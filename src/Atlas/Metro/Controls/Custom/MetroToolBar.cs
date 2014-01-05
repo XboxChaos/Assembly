5dee97dd0580fd15f9fb671ad172bc00e0b5856b
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-05 15:38:33 +0000

    started some work on WPF toolbars

---
+++ src/Atlas/Metro/Controls/Custom/MetroToolBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Atlas.Metro.Controls.Custom
+{
+	public class MetroToolBar : ToolBar
+	{
+		public static readonly DependencyProperty DropDownVisibilityProperty = DependencyProperty.Register(
+			"DropDownVisibility", typeof(Visibility), typeof(MetroToolBar));
+
+		public Visibility DropDownVisibility
+		{
+			get { return (Visibility)GetValue(DropDownVisibilityProperty); }
+			set { SetValue(DropDownVisibilityProperty, value); }
+		}
+	}
+}
