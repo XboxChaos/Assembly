8c360525de04311ad99d0453a8ea52a8397dfae2
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-14 13:57:48 +0000

    started work on engine memory

---
+++ src/Atlas/Models/EngineMemory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Atlas.Models
+{
+	public class EngineMemory
+	{
+		public ObservableCollection<EngineVersion> EngineVersions { get; set; }
+
+		public class EngineVersion
+		{
+			public double Version { get; set; }
+
+			public string VersionFriendly { get; set; }
+
+			public ObservableCollection<QuickOption> QuickOptions { get; set; }
+
+			public class QuickOption
+			{
+				public string Name { get; set; }
+
+				public string Description { get; set; }
+
+				public UInt32 Address { get; set; }
+
+				public bool IsToggle { get; set; }
+
+				public bool CarefulMode { get; set; }
+			}
+		}
+	}
+}
