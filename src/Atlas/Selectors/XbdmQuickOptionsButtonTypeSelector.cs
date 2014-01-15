8764c1f42b4cd67fd3e6baf7c799e11974bea851
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-15 04:37:19 +0000

    more work on quick options yolo

---
+++ src/Atlas/Selectors/XbdmQuickOptionsButtonTypeSelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Atlas.Models;
+
+namespace Atlas.Selectors
+{
+	public class XbdmQuickOptionsButtonTypeSelector : DataTemplateSelector
+	{
+		public override DataTemplate SelectTemplate(object item, DependencyObject container)
+		{
+			var element = container as FrameworkElement;
+			if (element == null || item == null || !(item is EngineMemory.EngineVersion.QuickOption)) 
+				return null;
+
+			var quickOption = item as EngineMemory.EngineVersion.QuickOption;
+
+			if (quickOption.IsToggle)
+				return element.FindResource("Toggle") as DataTemplate;
+
+			return element.FindResource("NotToggle") as DataTemplate;
+		}
+	}
+}
