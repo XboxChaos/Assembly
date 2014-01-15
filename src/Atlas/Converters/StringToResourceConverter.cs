4df996c2422cf47393154e7ad14adee59184b8b8
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-15 23:01:52 +0000

    Icons? more like Icans.

---
+++ src/Atlas/Converters/StringToResourceConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Atlas.Converters
+{
+	[ValueConversion(typeof (string), typeof (TemplateKey))]
+	public class StringToResourceConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return Application.Current.FindResource(value);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
