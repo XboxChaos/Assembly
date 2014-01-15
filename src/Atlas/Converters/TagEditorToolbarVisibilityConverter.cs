d6350e94222a7317241be1bcdf0e36f138161507
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-15 21:25:32 +0000

    closable tabs, toolbar work.

---
+++ src/Atlas/Converters/TagEditorToolbarVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using Atlas.Pages.CacheEditors;
+
+namespace Atlas.Converters
+{
+	[ValueConversion(typeof(ICacheEditor), typeof(Visibility))]
+	public class TagEditorToolbarVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var editor = value as ICacheEditor;
+			if (editor == null) return Visibility.Collapsed;
+
+			return (editor is TagEditor) ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
