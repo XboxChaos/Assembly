c59785d52fa6bd9a7a937f8b8a22c5540b8fdb59
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-16 15:16:57 +0000

    Tag Editor saving, and new dialog

---
+++ src/Atlas/Dialogs/Controls/MetroMessageBoxListWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Atlas.ViewModels.Dialog;
+
+namespace Atlas.Dialogs.Controls
+{
+	/// <summary>
+	/// Interaction logic for MetroMessageBoxListWindow.xaml
+	/// </summary>
+	public partial class MetroMessageBoxListWindow
+	{
+		public MetroMessageBoxListWindow(MessageBoxListViewModel viewModel)
+		{
+			Title = WindowTitle = viewModel.Title;
+			InitializeComponent();
+
+			DataContext = viewModel;
+			ListBox.ItemsSource = viewModel.Items;
+		}
+
+		private void WindowCloseButton_OnClick(object sender, RoutedEventArgs e) { Close(); }
+		private void CancelButton_Click(object sender, RoutedEventArgs e) { Close(); }
+
+		private void ContinueButton_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = true;
+			Close();
+		}
+	}
+}
---
+++ src/Atlas/Dialogs/MetroMessageBoxList.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using Atlas.Dialogs.Controls;
+using Atlas.ViewModels.Dialog;
+
+namespace Atlas.Dialogs
+{
+	public static class MetroMessageBoxList
+	{
+		/// <summary>
+		///     Shows a metro message box containing a list of items in it.
+		/// </summary>
+		/// <param name="title">The title of the Message Box</param>
+		/// <param name="message">The message to be displayed in the Message Box</param>
+		/// <param name="items">The items to be displayed in the message box.</param>
+		public static bool Show(string title, string message, IEnumerable<object> items)
+		{
+			var msgBox = new MetroMessageBoxListWindow(new MessageBoxListViewModel(title, message, items))
+			{
+				Owner = App.Storage.HomeWindow,
+				WindowStartupLocation = WindowStartupLocation.CenterOwner
+			};
+			msgBox.ShowDialog();
+			return msgBox.DialogResult ?? false;
+		}
+
+		/// <summary>
+		///     Show a Metro Message Box
+		/// </summary>
+		/// <param name="message">The message to be displayed in the Message Box</param>
+		/// <param name="items">The items to be displayed in the message box.</param>
+		public static bool Show(string message, IEnumerable<object> items)
+		{
+			return Show("Message Box - Assembly", message, items);
+		}
+	}
+}
