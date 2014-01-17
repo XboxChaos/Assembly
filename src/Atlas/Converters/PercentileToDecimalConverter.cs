f2a860904954f857d4b5264587b49350c1ef28c4
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-17 23:18:11 +0000

    plugin editor inline, worked on that

---
+++ src/Atlas/Converters/PercentileToDecimalConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Atlas.Converters
+{
+	[ValueConversion(typeof (string), typeof (float))]
+	public class PercentileToDecimalConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null) return 1.0f;
+			return float.Parse(((string)value).Replace("%", "")) / 100;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
