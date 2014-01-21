b555696a1d7480a9918eea12439b8a9d7f9903c4
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-21 00:19:15 +0000

    Plugin Revision Viewer

---
+++ src/Atlas/Dialogs/Controls/MetroPluginRevisionViewerWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Atlas.ViewModels.Dialog;
+
+namespace Atlas.Dialogs.Controls
+{
+	/// <summary>
+	/// Interaction logic for MetroMessageBoxWindow.xaml
+	/// </summary>
+	public partial class MetroPluginRevisionViewerWindow
+	{
+		public MetroPluginRevisionViewerWindow(PluginRevisionViewerViewModel viewModel)
+		{
+			InitializeComponent();
+
+			DataContext = viewModel;
+			Title = WindowTitle = viewModel.Title;
+		}
+
+		private void ExitButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void WindowCloseButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
---
+++ src/Atlas/Dialogs/MetroPluginRevisionViewer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using Atlas.Dialogs.Controls;
+using Atlas.ViewModels.Dialog;
+using Blamite.Plugins;
+
+namespace Atlas.Dialogs
+{
+	public static class MetroPluginRevisionViewer
+	{
+		public static void Show(string title, IList<PluginRevision> revisions)
+		{
+			var dialog = new MetroPluginRevisionViewerWindow(new PluginRevisionViewerViewModel(title, revisions))
+			{
+				Owner = App.Storage.HomeWindow,
+				WindowStartupLocation = WindowStartupLocation.CenterOwner
+			};
+			App.Storage.HomeWindowViewModel.ShowDialog();
+			dialog.ShowDialog();
+			App.Storage.HomeWindowViewModel.HideDialog();
+		}
+	}
+}
