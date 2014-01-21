4de9dfcf4e32d15d682944ec3424bb4707e8c579
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-21 16:33:51 +0000

    Script Editor. Well as much as their wall.

---
+++ src/Atlas/Converters/ScriptEditorToolbarVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using Atlas.Pages.CacheEditors;
+
+namespace Atlas.Converters
+{
+	[ValueConversion(typeof(ICacheEditor), typeof(Visibility))]
+	public class ScriptEditorToolbarVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var editor = value as ICacheEditor;
+			if (editor == null) return Visibility.Collapsed;
+
+			return (editor is ScriptEditor) ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
