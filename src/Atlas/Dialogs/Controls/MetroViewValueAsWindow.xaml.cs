a7c00397303890f3100ae9fb46d3ab82a8f40aef
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-22 17:58:16 +0000

    View value as should be finished

--- src/Atlas/Dialogs/Controls/MetroViewValueAsWindow.xaml.cs
+++ src/Atlas/Dialogs/Controls/MetroViewValueAsWindow.xaml.cs
@@ -23,5 +23,21 @@ namespace Atlas.Dialogs.Controls
 		}
 
 		private void WindowCloseButton_OnClick(object sender, RoutedEventArgs e) { Close(); }
+
+		#region Seeking/Reading Management
+
+		private void ResetSeekButton_Click(object sender, RoutedEventArgs e)
+		{
+			ViewModel.CacheOffset = ViewModel.CacheOffsetOriginal;
+			ViewModel.RefreshTagData();
+		}
+
+		private void RefreshDataButton_Click(object sender, RoutedEventArgs e) { ViewModel.RefreshTagData(); }
+
+		private void SeekForwardButton_Click(object sender, RoutedEventArgs e) { ViewModel.CacheOffset++; }
+
+		private void SeekBackButton_Click(object sender, RoutedEventArgs e) { ViewModel.CacheOffset--; }
+
+		#endregion
 	}
 }
