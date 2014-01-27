46ac1a6ec13ca7bf00e03a351c3bd4c9b746568d
Alex Reed (Xerax) <alexreed@outlook.com>
2014-01-27 04:25:23 +0000

    Split Buttons

---
+++ src/Atlas/Metro/Controls/Custom/MetroHighlightButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Atlas.Metro.Controls.Custom
+{
+	public class MetroHighlightButton : Button
+	{
+		public static readonly DependencyProperty IsHighlightedProperty = DependencyProperty.Register(
+			"IsHighlighted", typeof(bool), typeof(MetroHighlightButton), new PropertyMetadata(default(bool)));
+
+		public static readonly DependencyProperty IsFlippedProperty = DependencyProperty.Register(
+			"IsFlipped", typeof(bool), typeof(MetroHighlightButton), new PropertyMetadata(default(bool)));
+
+		public bool IsHighlighted
+		{
+			get { return (bool)GetValue(IsHighlightedProperty); }
+			set { SetValue(IsHighlightedProperty, value); }
+		}
+
+		public bool IsFlipped
+		{
+			get { return (bool)GetValue(IsFlippedProperty); }
+			set { SetValue(IsFlippedProperty, value); }
+		}
+	}
+}
