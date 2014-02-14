d49306e551c847629358b8835c8c4d5c4fff674e
Alex Reed (Xerax) <alexreed@outlook.com>
2014-02-14 03:24:22 +0000

    Le more work

---
+++ src/Atlas/Models/Cache/ExtractingResourceEntry.cs
@@ -0,0 +1,42 @@
+﻿using Blamite.Blam.Resources;
+using Blamite.Injection;
+
+namespace Atlas.Models.Cache
+{
+	public class ExtractingResourceEntry
+	{
+		public ExtractingResourceEntry(bool extract, Resource resource, ExtractedResourceInfo extractedResourceInfo)
+		{
+			Extract = extract;
+			ExtractedResourceInfo = extractedResourceInfo;
+			Resource = resource;
+		}
+
+		public bool Extract { get; set; }
+
+		public Resource Resource { get; set; }
+
+		public ExtractedResourceInfo ExtractedResourceInfo { get; set; }
+
+		public string PrimaryPageFilePath
+		{
+			get
+			{
+				if (Resource == null || Resource.Location == null || Resource.Location.PrimaryPage == null)
+					return "--";
+				
+				return Resource.Location.PrimaryPage.FilePath ?? "Local Resource";
+			}
+		}
+		public string SecondaryPageFilePath
+		{
+			get
+			{
+				if (Resource == null || Resource.Location == null || Resource.Location.SecondaryPage == null)
+					return "--";
+
+				return Resource.Location.SecondaryPage.FilePath ?? "Local Resource";
+			}
+		}
+	}
+}
