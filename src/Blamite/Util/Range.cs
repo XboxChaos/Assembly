d1017cd8b58d8a6aa50c4ce5ef9451c85f02c1d8
amd7 <amd@xboxchaos.com>
2014-10-29 20:44:53 -0400

    Added Range and RangeSet utility classes

---
+++ src/Blamite/Util/Range.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blamite.Util
+{
+    /// <summary>
+    /// A range of two comparable values. The starting value is inclusive, and the ending value is exclusive.
+    /// </summary>
+    public struct Range<T> where T : IComparable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Range&lt;T&gt;"/> struct.
+        /// </summary>
+        /// <param name="start">The starting value.</param>
+        /// <param name="end">The ending value (exclusive).</param>
+        public Range(T start, T end)
+        {
+            if (end.CompareTo(start) < 0)
+                throw new ArgumentException("end is less than start");
+            Start = start;
+            End = end;
+        }
+
+        /// <summary>
+        /// The starting value.
+        /// </summary>
+        public readonly T Start;
+
+        /// <summary>
+        /// The ending value (exclusive).
+        /// </summary>
+        public readonly T End;
+
+        /// <summary>
+        /// Determines whether or not a value is included in this range.
+        /// </summary>
+        /// <param name="val">The value to test.</param>
+        /// <returns><c>true</c> if the value is included in this range.</returns>
+        public bool Includes(T val)
+        {
+            return val.CompareTo(Start) >= 0 && val.CompareTo(End) < 0;
+        }
+
+        /// <summary>
+        /// Determines whether or not another range is entirely included in this range.
+        /// </summary>
+        /// <param name="other">The other range.</param>
+        /// <returns><c>true</c> if the other range is entirely contained inside this range.</returns>
+        public bool Includes(Range<T> other)
+        {
+            return other.Start.CompareTo(Start) >= 0 && other.End.CompareTo(End) <= 0;
+        }
+
+        /// <summary>
+        /// Determines whether or not this range intersects another range.
+        /// </summary>
+        /// <param name="other">The other range.</param>
+        /// <returns><c>true</c> if this range shares at least one value with the other range.</returns>
+        public bool Intersects(Range<T> other)
+        {
+            return other.Start.CompareTo(other.End) != 0 && Start.CompareTo(End) != 0
+                && ((other.Start.CompareTo(Start) >= 0 && other.Start.CompareTo(End) < 0)
+                || (other.End.CompareTo(Start) > 0 && other.End.CompareTo(End) <= 0)
+                || (other.Start.CompareTo(Start) <= 0 && other.End.CompareTo(End) >= 0));
+        }
+
+        /// <summary>
+        /// Determines whether or not a union can be successfully made between this range and another range.
+        /// That is, whether or not this range is adjacent to or intersects the other range.
+        /// </summary>
+        /// <param name="other">The other range.</param>
+        /// <returns><c>true</c> if a union can be successfully made between this range and the other range.</returns>
+        public bool CanUnionWith(Range<T> other)
+        {
+            return other.Start.CompareTo(End) <= 0 && other.End.CompareTo(Start) >= 0;
+        }
+
+        /// <summary>
+        /// Returns a range with the same ending value as this range but a different starting value.
+        /// </summary>
+        /// <param name="newStart">The starting value of the new range.</param>
+        /// <returns>The new range.</returns>
+        public Range<T> ResizeFront(T newStart)
+        {
+            return new Range<T>(newStart, End);
+        }
+
+        /// <summary>
+        /// Returns a range with the same starting value as this range but a different ending value.
+        /// </summary>
+        /// <param name="newEnd">The ending value of the new range.</param>
+        /// <returns>The new range.</returns>
+        public Range<T> ResizeBack(T newEnd)
+        {
+            return new Range<T>(Start, newEnd);
+        }
+
+        /// <summary>
+        /// Returns a range which is the union of this range with another range.
+        /// The ranges must intersect.
+        /// </summary>
+        /// <param name="other">The other range.</param>
+        /// <returns>A range which contains all values in both ranges.</returns>
+        public Range<T> UnionWith(Range<T> other)
+        {
+            if (!CanUnionWith(other))
+                throw new ArgumentException("The ranges do not intersect and are not adjacent");
+
+            var start = Start;
+            var end = End;
+            if (other.Start.CompareTo(Start) < 0)
+                start = other.Start;
+            if (other.End.CompareTo(End) > 0)
+                end = other.End;
+
+            return new Range<T>(start, end);
+        }
+    }
+}
---
+++ src/Blamite/Util/RangeSet.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blamite.Util
+{
+	/// <summary>
+	/// A set of ranges. Adjacent ranges will be combined with each other.
+	/// </summary>
+	/// <typeparam name="T">The type of value used to set the range bounds.</typeparam>
+	public class RangeSet<T> where T : IComparable
+	{
+		private static RangeStartComparer _startComparer = new RangeStartComparer();
+		private List<Range<T>> _ranges = new List<Range<T>>();
+
+		/// <summary>
+		/// Gets the ranges contained in the set.
+		/// </summary>
+		public IEnumerable<Range<T>> Ranges
+		{
+			get { return _ranges; }
+		}
+
+		/// <summary>
+		/// Inserts the specified range into the set.
+		/// It will be combined with other ranges as appropriate.
+		/// </summary>
+		/// <param name="range">The range to insert.</param>
+		public void Insert(Range<T> range)
+		{
+			var index = _ranges.BinarySearch(range, _startComparer);
+			if (index < 0)
+			{
+				// Start index not found
+				// Try to merge with the range before it
+				index = ~index;
+				if (index > 0 && TryMergeAndSimplify(range, index - 1))
+					return;
+			}
+
+			// Try to merge with the next range >= starting index
+			if (index < _ranges.Count && TryMergeAndSimplify(range, index))
+				return;
+
+			// Can't merge with anything - insert it
+			_ranges.Insert(index, range);
+		}
+
+		/// <summary>
+		/// Determines whether the set contains a range of values.
+		/// </summary>
+		/// <param name="range">The range of values to check.</param>
+		/// <returns><c>true</c> if the range is contained within any of the ranges in the set.</returns>
+		public bool ContainsRange(Range<T> range)
+		{
+			var index = _ranges.BinarySearch(range, _startComparer);
+			if (index < 0)
+			{
+				// Start index not found
+				// Flip it to get the index of the next highest range, and check if the one before it includes it
+				index = ~index;
+				return (index > 0 && _ranges[index - 1].Includes(range));
+			}
+			return _ranges[index].Includes(range);
+		}
+
+		/// <summary>
+		/// Tries the merge.
+		/// </summary>
+		/// <param name="first">The first range to merge.</param>
+		/// <param name="secondIndex">Index of the second range to merge.</param>
+		/// <param name="resultIndex">Index to store the result to.</param>
+		/// <param name="deleteSecond">If set to <c>true</c> and a merge occurs, the range at secondIndex will be removed.</param>
+		/// <returns><c>true</c> if a merge took place.</returns>
+		/// <exception cref="System.ArgumentOutOfRangeException">Thrown if either index is invalid.</exception>
+		private bool TryMerge(Range<T> first, int secondIndex, int resultIndex, bool deleteSecond)
+		{
+			if (secondIndex < 0 || secondIndex >= _ranges.Count)
+				throw new ArgumentOutOfRangeException("secondIndex");
+			if (resultIndex < 0 || resultIndex >= _ranges.Count)
+				throw new ArgumentOutOfRangeException("secondIndex");
+
+			var second = _ranges[secondIndex];
+			if (first.CanUnionWith(second))
+			{
+				_ranges[resultIndex] = first.UnionWith(second);
+				if (deleteSecond)
+					_ranges.RemoveAt(secondIndex);
+				return true;
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// Starting at an index, tries to merge the range at that index with as many subsequent ranges as possible.
+		/// </summary>
+		/// <param name="index">The index to start at.</param>
+		/// <exception cref="System.ArgumentOutOfRangeException">Thrown if the index is invalid.</exception>
+		private void Simplify(int index)
+		{
+			if (index < 0 || index >= _ranges.Count)
+				throw new ArgumentOutOfRangeException("index");
+
+			// Keep trying to merge, deleting the second range each time
+			var range = _ranges[index];
+			while (index < _ranges.Count - 1 && TryMerge(range, index + 1, index, true))
+				range = _ranges[index];
+		}
+
+		/// <summary>
+		/// Attempts to merge a range with the range at an index.
+		/// If the merge is successful, a simplify operation will be run starting from the index.
+		/// </summary>
+		/// <param name="range">The range to merge.</param>
+		/// <param name="index">The index to merge at.</param>
+		/// <returns><c>true</c> if at least one merge took place.</returns>
+		private bool TryMergeAndSimplify(Range<T> range, int index)
+		{
+			if (TryMerge(range, index, index, false))
+			{
+				Simplify(index);
+				return true;
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// Comparison function for ranges which compares their start indexes.
+		/// </summary>
+		private class RangeStartComparer : IComparer<Range<T>>
+		{
+			public int Compare(Range<T> x, Range<T> y)
+			{
+				return x.Start.CompareTo(y.Start);
+			}
+		}
+	}
+}
