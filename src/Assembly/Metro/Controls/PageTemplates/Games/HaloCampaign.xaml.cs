8456c62bf8ed144d7f745721538b6658a2c2437a
OrangeMohawk <orangemohawk121@gmail.com>
2014-11-01 04:43:33 -0400

    Transferred and updated .campaign editor UI
    from new_mapinfo

--- src/Assembly/Metro/Controls/PageTemplates/Games/HaloCampaign.xaml.cs
+++ src/Assembly/Metro/Controls/PageTemplates/Games/HaloCampaign.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using Assembly.Helpers;
 using Assembly.Metro.Controls.PageTemplates.Games.Components;
@@ -63,6 +66,148 @@ namespace Assembly.Metro.Controls.PageTemplates.Games
 			new LanguageEntry { Index = 16, Language = "Norwegian", LanguageShort = "nb" }
 		};
 
+		private List<TextBox> GetTextBoxes()
+		{
+			return new List<TextBox>
+			{
+				txtMap1,
+				txtMap2,
+				txtMap3,
+				txtMap4,
+				txtMap5,
+				txtMap6,
+				txtMap7,
+				txtMap8,
+				txtMap9,
+				txtMap10,
+				txtMap11,
+				txtMap12,
+				txtMap13,
+				txtMap14,
+				txtMap15,
+				txtMap16,
+				txtMap17,
+				txtMap18,
+				txtMap19,
+				txtMap20,
+				txtMap21,
+				txtMap22,
+				txtMap23,
+				txtMap24,
+				txtMap25,
+				txtMap26,
+				txtMap27,
+				txtMap28,
+				txtMap29,
+				txtMap30,
+				txtMap31,
+				txtMap32,
+				txtMap33,
+				txtMap34,
+				txtMap35,
+				txtMap36,
+				txtMap37,
+				txtMap38,
+				txtMap39,
+				txtMap40,
+				txtMap41,
+				txtMap42,
+				txtMap43,
+				txtMap44,
+				txtMap45,
+				txtMap46,
+				txtMap47,
+				txtMap48,
+				txtMap49,
+				txtMap50,
+				txtMap51,
+				txtMap52,
+				txtMap53,
+				txtMap54,
+				txtMap55,
+				txtMap56,
+				txtMap57,
+				txtMap58,
+				txtMap59,
+				txtMap60,
+				txtMap61,
+				txtMap62,
+				txtMap63,
+				txtMap64
+			};
+		}
+
+		private List<ToggleButton> GetToggleButtons()
+		{
+			return new List<ToggleButton>
+			{
+				cbMap1,
+				cbMap2,
+				cbMap3,
+				cbMap4,
+				cbMap5,
+				cbMap6,
+				cbMap7,
+				cbMap8,
+				cbMap9,
+				cbMap10,
+				cbMap11,
+				cbMap12,
+				cbMap13,
+				cbMap14,
+				cbMap15,
+				cbMap16,
+				cbMap17,
+				cbMap18,
+				cbMap19,
+				cbMap20,
+				cbMap21,
+				cbMap22,
+				cbMap23,
+				cbMap24,
+				cbMap25,
+				cbMap26,
+				cbMap27,
+				cbMap28,
+				cbMap29,
+				cbMap30,
+				cbMap31,
+				cbMap32,
+				cbMap33,
+				cbMap34,
+				cbMap35,
+				cbMap36,
+				cbMap37,
+				cbMap38,
+				cbMap39,
+				cbMap40,
+				cbMap41,
+				cbMap42,
+				cbMap43,
+				cbMap44,
+				cbMap45,
+				cbMap46,
+				cbMap47,
+				cbMap48,
+				cbMap49,
+				cbMap50,
+				cbMap51,
+				cbMap52,
+				cbMap53,
+				cbMap54,
+				cbMap55,
+				cbMap56,
+				cbMap57,
+				cbMap58,
+				cbMap59,
+				cbMap60,
+				cbMap61,
+				cbMap62,
+				cbMap63,
+				cbMap64
+			};
+		}
+
 		private PureBLF _blf;
 		private Campaign _campaign;
 		private bool _startEditing;
@@ -105,10 +250,15 @@ namespace Assembly.Metro.Controls.PageTemplates.Games
 					// Load Languages
 					LoadLanguages();
 
+					// Load Map IDs
+					LoadMapIDs();
+
+					// Load Unlock Bytes
+					LoadUnlockBytes();
+
 					// Update UI
 					_startEditing = true;
 					cbLanguages.SelectedIndex = 0;
-					//MessageBox.Show(_campaign.HaloCampaign.MapIDs[0].ToString());
 
 					if (App.AssemblyStorage.AssemblySettings.StartpageHideOnLaunch)
 						App.AssemblyStorage.AssemblySettings.HomeWindow.ExternalTabClose(Home.TabGenre.StartPage);
@@ -180,6 +330,15 @@ namespace Assembly.Metro.Controls.PageTemplates.Games
 			_campaign.HaloCampaign.MapNames[cbLanguages.SelectedIndex] = txtMapName.Text;
 			_campaign.HaloCampaign.MapDescriptions[cbLanguages.SelectedIndex] = txtMapDesc.Text;
 
+			if (MapIDsError() == true)
+				return;
+
+			// Update Map IDs
+			UpdateMapIDs();
+
+			// Update Unlock Bytes
+			UpdateUnlockBytes();
+
 			// Write all changes to file
 			_campaign.UpdateCampaign();
 			Close();
@@ -212,6 +371,72 @@ namespace Assembly.Metro.Controls.PageTemplates.Games
 					break;
 			}
 			cbLanguages.DataContext = _languageset;
+		}
+
+		private void LoadMapIDs()
+		{
+			foreach (var textbox in GetTextBoxes())
+				textbox.Text = _campaign.HaloCampaign.MapIDs[int.Parse((string)textbox.Tag)].ToString(CultureInfo.InvariantCulture);
+		}
+
+		private void LoadUnlockBytes()
+		{
+			if (_campaign.HaloCampaign.Game == Campaign.GameIdentifier.Halo4)
+			{
+				lblToggleNote.Visibility = Visibility.Visible;
+				foreach (var toggleButton in GetToggleButtons())
+					toggleButton.IsChecked = Convert.ToBoolean(_campaign.HaloCampaign.UnlockBytes[int.Parse((string) toggleButton.Tag)]);
+			}
+			else
+			{
+				foreach (var toggleButton in GetToggleButtons())
+					toggleButton.IsEnabled = false;
+			}
+		}
+
+		// Check if any Map IDs are invalid
+		private bool MapIDsError()
+		{
+			for (int i = 1; i <= 7; i += 2)
+			{
+				var sp = (StackPanel)gridMapIds.Children[i];
+				if (sp.Children.Cast<TextBox>().Any(tb => Equals(tb.BorderBrush, FindResource("ExtryzeAccentBrush"))))
+				{
+					Close();
+					MetroMessageBox.Show("Campaign Not Saved", "The Campaign could not be saved because one or more of the Map IDs is invalid.");
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private void UpdateMapIDs()
+		{
+			foreach (var textbox in GetTextBoxes())
+				_campaign.HaloCampaign.MapIDs[int.Parse((string) textbox.Tag)] = int.Parse(textbox.Text);
+		}
+
+		private void UpdateUnlockBytes()
+		{
+			if (_campaign.HaloCampaign.Game != Campaign.GameIdentifier.Halo4)
+				return;
+
+			foreach (var toggleButton in GetToggleButtons())
+				_campaign.HaloCampaign.UnlockBytes[int.Parse((string)toggleButton.Tag)] = Convert.ToByte(toggleButton.IsChecked);
+		}
+
+		private void MapIDValidityCheck(object sender, TextChangedEventArgs e)
+		{
+			var textbox = sender as TextBox;
+
+			if (textbox == null)
+				return;
+
+			Int32 tmp32;
+			if (Int32.TryParse(textbox.Text, out tmp32))
+				textbox.BorderBrush = (Brush)new BrushConverter().ConvertFromString("#FF595959");
+			else
+				textbox.BorderBrush = (Brush)FindResource("ExtryzeAccentBrush");
 		}
 
 		public class LanguageEntry
