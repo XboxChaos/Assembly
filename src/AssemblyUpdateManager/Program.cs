daf56a6a7d9e1bef453af08c02a316bfea8ec8f8
amd7 <amd@xboxchaos.com>
2014-12-21 01:28:53 -0600

    AssemblyUpdateManager now backs up layouts and plugins

--- src/AssemblyUpdateManager/Program.cs
+++ src/AssemblyUpdateManager/Program.cs
@@ -34,6 +34,16 @@ namespace AssemblyUpdateManager
 				{
 				}
 
+				// Back up formats and plugins
+				try
+				{
+					DirectoryCopy("Formats", @"Backup\Formats", true);
+					DirectoryCopy("Plugins", @"Backup\Plugins", true);
+				}
+				catch
+				{
+				}
+
 				// Extract the update zip
 				var fz = new FastZip();
 				fz.CreateEmptyDirectories = true;
@@ -69,6 +79,42 @@ namespace AssemblyUpdateManager
 
 			// Launch "The New iPa... Assembly"
 			Process.Start("Assembly://post-update");
+		}
+
+		// http://msdn.microsoft.com/en-us/library/bb762914%28v=vs.110%29.aspx
+		private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+		{
+			// Get the subdirectories for the specified directory.
+			var dir = new DirectoryInfo(sourceDirName);
+			var dirs = dir.GetDirectories();
+
+			if (!dir.Exists)
+			{
+				throw new DirectoryNotFoundException(
+					"Source directory does not exist or could not be found: "
+					+ sourceDirName);
+			}
+
+			// If the destination directory doesn't exist, create it. 
+			if (!Directory.Exists(destDirName))
+				Directory.CreateDirectory(destDirName);
+
+			// Get the files in the directory and copy them to the new location.
+			var files = dir.GetFiles();
+			foreach (var file in files)
+			{
+				var temppath = Path.Combine(destDirName, file.Name);
+				file.CopyTo(temppath, false);
+			}
+
+			// If copying subdirectories, copy them and their contents to new location. 
+			if (!copySubDirs)
+				return;
+			foreach (var subdir in dirs)
+			{
+				var temppath = Path.Combine(destDirName, subdir.Name);
+				DirectoryCopy(subdir.FullName, temppath, true);
+			}
 		}
 	}
 }
