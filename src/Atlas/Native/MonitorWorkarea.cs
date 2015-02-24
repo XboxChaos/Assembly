0e7170ee434fac7da1374833ce872f938a027fc6
Chad Zawistowski <chzawist@microsoft.com>
2015-02-24 11:48:37 -0800

    Use System.Drawing instead of redefining types

--- src/Atlas/Native/MonitorWorkarea.cs
+++ src/Atlas/Native/MonitorWorkarea.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Runtime.InteropServices;
 
 namespace Atlas.Native
@@ -8,8 +9,6 @@ namespace Atlas.Native
         [DllImport("user32")]
         internal static extern bool GetMonitorInfo(IntPtr hMonitor, MonitorInfo lpmi);
 
-        /// <summary>
-        /// </summary>
         [DllImport("User32")]
         internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
 
@@ -23,147 +22,16 @@ namespace Atlas.Native
             public Point ptMaxTrackSize;
         };
 
-        /// <summary>
-        /// </summary>
+        /// <remarks>
+        /// This is only a class so cbSize can be initialized properly.
+        /// </remarks>
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
         public class MonitorInfo
         {
-            public MonitorInfo()
-            {
-                cbSize = (uint) Marshal.SizeOf(typeof (MonitorInfo));
-            }
-
-            public uint cbSize;
-
-            /// <summary>
-            /// </summary>
-            public Rect rcMonitor = new Rect();
-
-            /// <summary>
-            /// </summary>
-            public Rect rcWork = new Rect();
-
-            public uint dwFlags;
-        }
-
-        /// <summary>
-        ///     POINT aka POINTAPI
-        /// </summary>
-        [StructLayout(LayoutKind.Sequential)]
-        public struct Point
-        {
-            /// <summary>
-            ///     x coordinate of point.
-            /// </summary>
-            public int x;
-
-            /// <summary>
-            ///     y coordinate of point.
-            /// </summary>
-            public int y;
-        }
-
-
-        /// <summary> Win32 </summary>
-        [StructLayout(LayoutKind.Sequential, Pack = 0)]
-        public struct Rect
-        {
-            /// <summary> Win32 </summary>
-            public int left;
-
-            /// <summary> Win32 </summary>
-            public int top;
-
-            /// <summary> Win32 </summary>
-            public int right;
-
-            /// <summary> Win32 </summary>
-            public int bottom;
-
-            /// <summary> Win32 </summary>
-            public static readonly Rect Empty = new Rect();
-
-            /// <summary> Win32 </summary>
-            public int Width
-            {
-                get { return Math.Abs(right - left); } // Abs needed for BIDI OS
-            }
-
-            /// <summary> Win32 </summary>
-            public int Height
-            {
-                get { return bottom - top; }
-            }
-
-            /// <summary> Win32 </summary>
-            public Rect(int left, int top, int right, int bottom)
-            {
-                this.left = left;
-                this.top = top;
-                this.right = right;
-                this.bottom = bottom;
-            }
-
-
-            /// <summary> Win32 </summary>
-            public Rect(Rect rcSrc)
-            {
-                left = rcSrc.left;
-                top = rcSrc.top;
-                right = rcSrc.right;
-                bottom = rcSrc.bottom;
-            }
-
-            /// <summary> Win32 </summary>
-            public bool IsEmpty
-            {
-                get
-                {
-                    // BUGBUG : On Bidi OS (hebrew arabic) left > right
-                    return left >= right || top >= bottom;
-                }
-            }
-
-            /// <summary> Return a user friendly representation of this struct </summary>
-            public override string ToString()
-            {
-                if (this == Empty)
-                {
-                    return "RECT {Empty}";
-                }
-                return "RECT { left : " + left + " / top : " + top + " / right : " + right + " / bottom : " + bottom +
-                       " }";
-            }
-
-            /// <summary> Determine if 2 RECT are equal (deep compare) </summary>
-            public override bool Equals(object obj)
-            {
-                if (!(obj is System.Windows.Rect))
-                {
-                    return false;
-                }
-                return (this == (Rect) obj);
-            }
-
-            /// <summary>Return the HashCode for this struct (not garanteed to be unique)</summary>
-            public override int GetHashCode()
-            {
-                return left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
-            }
-
-
-            /// <summary> Determine if 2 RECT are equal (deep compare)</summary>
-            public static bool operator ==(Rect rect1, Rect rect2)
-            {
-                return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right &&
-                        rect1.bottom == rect2.bottom);
-            }
-
-            /// <summary> Determine if 2 RECT are different(deep compare)</summary>
-            public static bool operator !=(Rect rect1, Rect rect2)
-            {
-                return !(rect1 == rect2);
-            }
+            public uint cbSize = (uint)Marshal.SizeOf(typeof(MonitorInfo));
+            public Rectangle rcMonitor = new Rectangle();
+            public Rectangle rcWork = new Rectangle();
+            public uint dwFlags = 0;
         }
     }
 }
