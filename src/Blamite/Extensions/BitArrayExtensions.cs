2056c4ae4218b29ab2d8d65d4f19e4fdb4d7ba86
Lord-Zedd <Lord-Zedd@users.noreply.github.com>
2017-10-30 22:27:40 -0400

    fix ToIntArray

--- src/Blamite/Extensions/BitArrayExtensions.cs
+++ src/Blamite/Extensions/BitArrayExtensions.cs
@@ -24,17 +24,21 @@ namespace Blamite.Extensions
 
 		public static int[] ToIntArray(this BitArray bits)
 		{
+			//round bits up
+			if (bits.Length % 32 != 0)
+				bits.Length += 32 - bits.Length % 32;
+
 			var bytes = bits.ToByteArray();
 			var ints = new List<int>();
 
 			for(var i = 0; i < bytes.Length; i += 4)
-				ints.Add(Convert.ToInt32(new []
+				ints.Add(BitConverter.ToInt32(new byte[]
 				{
 					bytes[i],
 					bytes[i + 1],
 					bytes[i + 2],
 					bytes[i + 3]
-				}));
+				}, 0));
 
 			return ints.ToArray();
 		}
