c05b2d9957aae9c9e852311340ff865112be8111
Lord-Zedd <Lord-Zedd@users.noreply.github.com>
2018-07-31 05:27:23 -0400

    tweaks to tagsearchbox
    fixes styling (and spaces eww), adds datum searching, and a tip for
    injected tags

--- src/Assembly/Metro/Dialogs/ControlDialogs/TagSearchBox.xaml.cs
+++ src/Assembly/Metro/Dialogs/ControlDialogs/TagSearchBox.xaml.cs
@@ -6,77 +6,89 @@ using System.Text.RegularExpressions;
 
 namespace Assembly.Metro.Dialogs.ControlDialogs
 {
-    /// <summary>
-    ///     Interaction logic for MessageBoxList.xaml
-    /// </summary>
-    public partial class TagValueSearcher
-    {
-        public int SelectedTagIndex { get; private set; }
-        public TagEntry SelectedTag { get; private set; }
-        private Collection<TagEntry> AvailableTags { get; set; }
-        private TagClass TagClass { get; set; }
+	/// <summary>
+	///     Interaction logic for MessageBoxList.xaml
+	/// </summary>
+	public partial class TagValueSearcher
+	{
+		public int SelectedTagIndex { get; private set; }
+		public TagEntry SelectedTag { get; private set; }
+		private Collection<TagEntry> AvailableTags { get; set; }
+		private TagClass TagClass { get; set; }
 
-        public TagValueSearcher(TagClass tagClass)
-        {
-            InitializeComponent();
-            TagClass = tagClass;
+		public TagValueSearcher(TagClass tagClass)
+		{
+			InitializeComponent();
+			TagClass = tagClass;
 
-            string tagClassName = tagClass.TagClassMagic;
-            Title = $"[{tagClassName}] Tag Search";
-            lblTitle.Text = Title;
+			string tagClassName = tagClass.TagClassMagic;
+			Title = $"[{tagClassName}] Tag Search";
+			lblTitle.Text = Title;
 
-            UpdateSearchResults();
+			UpdateSearchResults();
 
-        }
+		}
 
-        private void btnOK_Click(object sender, RoutedEventArgs e)
-        {
-            ExitSuccessfully();
-        }
+		private void btnOK_Click(object sender, RoutedEventArgs e)
+		{
+			ExitSuccessfully();
+		}
 
-        private void btnCancel_Click(object sender, RoutedEventArgs e)
-        {
-            DialogResult = false;
-            Close();
-        }
+		private void btnCancel_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = false;
+			Close();
+		}
 
-        private void UpdateSearchResults()
-        {
-            listTagSearchResults.Items.Clear();
-            foreach (TagEntry tag in TagClass.Children)
-                if (tag != null && tag.TagFileName.ToLower().Contains(textSearch.Text.ToLower()))
-                    listTagSearchResults.Items.Add(tag);
-        }
+		private void UpdateSearchResults()
+		{
+			listTagSearchResults.Items.Clear();
+			foreach (TagEntry tag in TagClass.Children)
+				if (tag != null)
+				{
+					if (textSearch.Text.StartsWith("0x"))
+					{
+						string searchHex = textSearch.Text.Substring(2);
+						if (tag.RawTag.Index.ToString().ToLower().Substring(2).Contains(searchHex))
+						{
+							listTagSearchResults.Items.Add(tag);
+							continue;
+						}	
+					}
+					if (tag.TagFileName.ToLower().Contains(textSearch.Text.ToLower()))
+						listTagSearchResults.Items.Add(tag);
+				}
+		}
 
-        private void listTagSearchResults_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            UpdateSelectedTag();
-        }
+		private void listTagSearchResults_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			UpdateSelectedTag();
+		}
 
-        private void textSearch_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
-        {
-            UpdateSearchResults();
-        }
+		private void textSearch_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+		{
+			UpdateSearchResults();
+		}
 
-        private void UpdateSelectedTag()
-        {
-            SelectedTag = (TagEntry)listTagSearchResults.SelectedItem;
-            SelectedTagIndex = TagClass.Children.IndexOf(SelectedTag);
-        }
+		private void UpdateSelectedTag()
+		{
+			SelectedTag = (TagEntry)listTagSearchResults.SelectedItem;
+			SelectedTagIndex = TagClass.Children.IndexOf(SelectedTag);
+		}
 
-        private void ExitSuccessfully()
-        {
-            DialogResult = true;
-            Close();
-        }
+		private void ExitSuccessfully()
+		{
+			DialogResult = true;
+			Close();
+		}
 
-        private void listTagSearchResults_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            if (listTagSearchResults.SelectedItem != null)
-            {
-                UpdateSelectedTag();
-                ExitSuccessfully();
-            }
-        }
-    }
+		private void listTagSearchResults_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			if (listTagSearchResults.SelectedItem != null)
+			{
+				UpdateSelectedTag();
+				ExitSuccessfully();
+			}
+		}
+	}
 }
