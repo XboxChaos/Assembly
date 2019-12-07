cc8f028a9d74ab240de028191c103292c317ddb7
Alexander Forbes-Reed <git@alx.red>
2019-12-07 13:13:50 +0000

    And the code for the snub

---
+++ src/XBDMCommunicator/XDevkitSnub/SnubException.cs
@@ -0,0 +1,7 @@
+﻿namespace XBDMCommunicator.XDevkitSnub
+{
+	public static class SnubException
+	{
+		public static string Message { get { return "Must be build with XDevkit to run this."; } }
+	}
+}
