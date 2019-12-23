cdd6ba14d11d1bcb1849d40cc050f6e350ff99ac
Alex Forbes-Reed <git@alx.red>
2019-12-23 17:29:15 +0000

    Patch loader now checks for unknown versions (#249)
    * Throw exception if patch versions are too new
    
    * Fix up exception messages

--- src/Blamite/Patching/AssemblyPatchLoader.cs
+++ src/Blamite/Patching/AssemblyPatchLoader.cs
@@ -34,21 +34,21 @@ namespace Blamite.Patching
 						break;
 
 					case "segm":
-						ReadSegmentChanges(reader, result);
+						ReadSegmentChanges(reader, container.BlockVersion, result);
 						break;
 
 					case "blfc":
-						ReadBlfInfo(reader, result);
+						ReadBlfInfo(reader, container.BlockVersion, result);
 						break;
 
 						#region Deprecated
 
 					case "meta":
-						ReadMetaChanges(reader, result);
+						ReadMetaChanges(reader, container.BlockVersion, result);
 						break;
 
 					case "locl":
-						ReadLocaleChanges(reader, result);
+						ReadLocaleChanges(reader, container.BlockVersion, result);
 						break;
 
 						#endregion Deprecated
@@ -59,6 +59,9 @@ namespace Blamite.Patching
 
 		private static void ReadPatchInfo(IReader reader, byte version, Patch output)
 		{
+			if (version > 3)
+				throw new NotSupportedException("Unrecognized \"titl\" block version");
+
 			// Version 0 (all versions)
 			output.MapID = reader.ReadInt32();
 			output.MapInternalName = reader.ReadAscii();
@@ -87,7 +90,7 @@ namespace Blamite.Patching
 				output.OutputName = "";
 
 			// Version 3
-			if (version >= 3)
+			if (version == 3)
 			{
 				output.PC = reader.ReadByte() == 1;
 				output.BuildString = reader.ReadAscii();
@@ -96,8 +99,11 @@ namespace Blamite.Patching
 				output.BuildString = "";
 		}
 
-		private static void ReadSegmentChanges(IReader reader, Patch output)
+		private static void ReadSegmentChanges(IReader reader, byte version, Patch output)
 		{
+			if (version > 0)
+				throw new NotSupportedException("Unrecognized \"segm\" block version");
+
 			// Version 0 (all versions)
 			byte numChanges = reader.ReadByte();
 			for (int i = 0; i < numChanges; i++)
@@ -138,8 +144,11 @@ namespace Blamite.Patching
 			return result;
 		}
 
-		private static void ReadBlfInfo(IReader reader, Patch output)
+		private static void ReadBlfInfo(IReader reader, byte version, Patch output)
 		{
+			if (version > 0)
+				throw new NotSupportedException("Unrecognized \"blfc\" block version");
+
 			// Version 0 (all versions)
 			var targetGame = (TargetGame) reader.ReadByte();
 			string mapInfoFileName = reader.ReadAscii();
@@ -159,13 +168,19 @@ namespace Blamite.Patching
 
 		#region Deprecated
 
-		private static void ReadMetaChanges(IReader reader, Patch output)
+		private static void ReadMetaChanges(IReader reader, byte version, Patch output)
 		{
+			if (version > 0)
+				throw new NotSupportedException("Unrecognized \"meta\" block version");
+
 			output.MetaChanges.AddRange(ReadDataChanges(reader));
 		}
 
-		private static void ReadLocaleChanges(IReader reader, Patch output)
+		private static void ReadLocaleChanges(IReader reader, byte version, Patch output)
 		{
+			if (version > 0)
+				throw new NotSupportedException("Unrecognized \"locl\" block version");
+
 			// Read language changes
 			byte numLanguageChanges = reader.ReadByte();
 			for (byte i = 0; i < numLanguageChanges; i++)
