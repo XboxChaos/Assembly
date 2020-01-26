206f1a045ab4c3f597e7a3a9d3de1837ccd9f63e
Aaron Dierking <aarondierking7@gmail.com>
2020-01-25 19:22:28 -0800

    Clarify licensing

--- src/XBDMCommunicator/Properties/AssemblyInfo.cs
+++ src/XBDMCommunicator/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Assembly XBDM Communicator")]
-[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyCopyright("Copyright © 2012-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
