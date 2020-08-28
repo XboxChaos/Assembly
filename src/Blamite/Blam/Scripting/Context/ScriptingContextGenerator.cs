e39a870c58c01dffa11e24185612b299d9845122
SnipeStyle <snipestylev2@gmail.com>
2020-08-28 23:18:00 +0200

    Structure Reader Update, Unit Seat Mapping Calculation, Context Loading
    Updated the structure reading framework to accept tagrefs, tag blocks and stringids.
    Created a new scripting context loader using the structure framework.
    Figured out how to calculate seat mapping names and count values.

---
+++ src/Blamite/Blam/Scripting/Context/ScriptingContextGenerator.cs
@@ -0,0 +1,202 @@
+﻿using Antlr4.Runtime;
+using Blamite.IO;
+using Blamite.Serialization;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blamite.Blam.Scripting.Context
+{
+    public static class ScriptingContextGenerator
+    {
+        public static ScriptingContextCollection GenerateContext(IReader reader, ICacheFile cache, EngineDescription buildinfo)
+        {
+            var result = new ScriptingContextCollection();
+            var scnrTag = cache.Tags.FindTagByGroup("scnr");
+            var mdlgTag = cache.Tags.FindTagByGroup("mdlg");
+            
+            if (scnrTag != null && buildinfo.Layouts.HasLayout("scnr script context"))
+            {
+                var scnrLayout = buildinfo.Layouts.GetLayout("scnr script context");
+                reader.SeekTo(scnrTag.MetaLocation.AsOffset());
+                var scnrValues = CacheStructureReader.ReadStructure(reader, cache, buildinfo, scnrLayout);
+                FillContextCollection(reader, cache, buildinfo, scnrValues, result);
+            }
+
+            if(mdlgTag != null && buildinfo.Layouts.HasLayout("mdlg script context"))
+            {
+                var mdlgLayout = buildinfo.Layouts.GetLayout("mdlg script context");
+                reader.SeekTo(mdlgTag.MetaLocation.AsOffset());
+                var mdlgValues = CacheStructureReader.ReadStructure(reader, cache, buildinfo, mdlgLayout);
+                FillContextCollection(reader, cache, buildinfo, mdlgValues, result);
+            }
+
+            return result;
+        }
+
+        private static void FillContextCollection(IReader reader, ICacheFile cache, EngineDescription buildinfo, StructureValueCollection values, ScriptingContextCollection collection)
+        {
+            var tagBlocks = values.GetTagBlocks();
+
+            foreach(var block in tagBlocks)
+            {
+                if(block.Value.Length == 0)
+                {
+                    continue;
+                }
+
+                if(block.Key == "unit_seat_mapping")
+                {
+                    HandleUnitSeatMappings(reader, cache, buildinfo, block, collection);
+                }
+                else if(IsWrapperBlock(block))
+                {
+                    HandleWrapperBlock(block, collection);
+                }
+                else
+                {
+                    collection.AddObjectGroup(CreateContextBlock(block, false));
+                }
+            }
+        }
+
+        private static bool IsWrapperBlock(KeyValuePair<string, StructureValueCollection[]> block)
+        {
+            if(!block.Value[0].HasStringID("Name") && !block.Value[0].HasString("Name"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static void HandleWrapperBlock(KeyValuePair<string, StructureValueCollection[]> block, ScriptingContextCollection collection)
+        {
+            if (block.Value.Length != 1)
+            {
+                throw new ArgumentException($"The wrapper context block {block.Key} doesn't have exactly one element.");
+            }
+
+            var innerBlocks = block.Value[0].GetTagBlocks();
+
+            foreach (var inner in innerBlocks)
+            {
+                collection.AddObjectGroup(CreateContextBlock(inner, false));
+            }
+        }
+
+        private static ScriptingContextBlock CreateContextBlock(KeyValuePair<string, StructureValueCollection[]> block, bool isChildBlock)
+        {
+            IEnumerable<ScriptingContextObject> objects = block.Value.Select((values, index) => CreateContextObject(index, block.Key, isChildBlock, values));
+            return new ScriptingContextBlock(block.Key, objects);
+        }
+
+        private static ScriptingContextObject CreateContextObject(int index, string group, bool isChildObject, StructureValueCollection values)
+        {
+            string name = values.HasStringID("Name") ? values.GetStringID("Name") : values.GetString("Name");
+            var result = new ScriptingContextObject(name, index, group, isChildObject);
+
+            var childBlocks = values.GetTagBlocks();
+            if(childBlocks.Length > 0)
+            {
+                foreach(var block in childBlocks)
+                {
+                    result.AddChildBlock(CreateContextBlock(block, true));
+                }
+            }
+
+            return result;
+        }
+
+        private static void HandleUnitSeatMappings(IReader reader, ICacheFile cache, EngineDescription buildInfo, 
+            KeyValuePair<string, StructureValueCollection[]> block, ScriptingContextCollection collection)
+        {
+            string lastMappingName = "";
+            short firstOccurenceIndex = 0;
+            short count = 0;
+
+            for (short mappingIndex = 0; mappingIndex < block.Value.Length; mappingIndex++)
+            {
+                var values = block.Value[mappingIndex];
+                ITag unit = values.GetTagReference("Unit");
+                int seatIndeces1 = (int)values.GetInteger("Seats 1");
+                int seatIndeces2 = (int)values.GetInteger("Seats 2");
+
+                List<int> seatIndices = GetSeatIndices(seatIndeces1, seatIndeces2);
+
+                var vehiLayout = buildInfo.Layouts.GetLayout("vehi seats");
+                reader.SeekTo(unit.MetaLocation.AsOffset());
+                var vehiValues = CacheStructureReader.ReadStructure(reader, cache, buildInfo, vehiLayout);
+                var seatsBlock = vehiValues.GetTagBlock("seats");
+
+                string mappingName = GetVehicleMappingName(seatsBlock, seatIndices);
+
+                if(mappingName != lastMappingName)
+                {
+                    // Don't add a unit seat mapping to the collection in the first iteration.
+                    if(mappingIndex > 0)
+                    {
+                        var mappingObject = new UnitSeatMapping(firstOccurenceIndex, count, lastMappingName);
+                        collection.AddUnitSeatMapping(mappingObject);
+                    }
+
+                    // Reset
+                    lastMappingName = mappingName;
+                    firstOccurenceIndex = mappingIndex;
+                    count = 1;
+                }
+                else
+                {
+                    count++;
+
+                    // If the last iteration contained a duplicate seat mapping, add it to the collection.
+                    if (mappingIndex == block.Value.Length - 1)
+                    {
+                        var mappingObject = new UnitSeatMapping(firstOccurenceIndex, count, lastMappingName);
+                        collection.AddUnitSeatMapping(mappingObject);
+                    }
+                }
+            }
+        }
+
+        private static List<int> GetSeatIndices(int seats1, int seats2)
+        {
+            var bits = new BitArray(new int[] { seats1, seats2 });
+            List<int> result = new List<int>();
+
+            for (int seatIndex = 0; seatIndex < bits.Length; seatIndex++)
+            {
+                if (bits[seatIndex])
+                {
+                    result.Add(seatIndex);
+                }
+            }
+
+            return result;
+        }
+
+        private static string GetVehicleMappingName(StructureValueCollection[] seats, IEnumerable<int> indeces)
+        {
+            List<string> names = new List<string>();
+
+            // Retrieve the names of all included seats.
+            foreach(int index in indeces)
+            {
+                string seatName = seats[index].GetStringID("Name");
+                names.Add(seatName);
+            }
+
+            // Get the longest common prefix. This will be the mapping name.
+            var result = names.Min().TakeWhile((c, i) => names.All(s => s[i] == c)).ToArray();
+            return new string(result).TrimEnd('_');
+        }
+
+    }
+}
---
+++ src/Blamite/Serialization/CacheStructureReader.cs
@@ -0,0 +1,103 @@
+﻿using Blamite.Blam;
+using Blamite.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blamite.Serialization
+{
+    public class CacheStructureReader : StructureReader, ICacheStructureLayoutVisitor
+    {
+		private readonly ICacheFile _cache;
+		private readonly EngineDescription _buildInfo;
+
+        public CacheStructureReader(IReader reader, ICacheFile cache, EngineDescription buildInfo) : base(reader)
+        {
+			_cache = cache;
+			_buildInfo = buildInfo;
+        }
+
+		public void VisitTagBlockField(string name, int offset, StructureLayout layout)
+		{
+			SeekReader(offset);
+			var tagBlockLayout = _buildInfo.Layouts.GetLayout("tag block");
+			var blockHeader = ReadStructure(_reader, _cache, _buildInfo, tagBlockLayout);
+			uint pointer = (uint)blockHeader.GetInteger("pointer");
+			int count = (int)blockHeader.GetInteger("entry count");
+			long expandedPointer = _cache.PointerExpander.Expand(pointer);
+
+			if (count == 0 || !_cache.MetaArea.ContainsPointer(expandedPointer))
+			{
+				_collection.SetTagBlock(name, new StructureValueCollection[0]);
+			}
+			else
+			{
+				var elements = new StructureValueCollection[count];
+				int blockOffset = _cache.MetaArea.PointerToOffset(expandedPointer);
+				_reader.SeekTo(blockOffset);
+				for (int i = 0; i < count; i++)
+				{
+					elements[i] = ReadStructure(_reader, _cache, _buildInfo, layout);
+				}
+				_collection.SetTagBlock(name, elements);
+			}
+		}
+
+		public void VisitStringIDField(string name, int offset)
+        {
+			SeekReader(offset);
+			uint value = _reader.ReadUInt32();
+			string id = _cache.StringIDs.GetString(new StringID(value));
+			_collection.SetStringID(name, id);
+			_offset += 4;
+        }
+
+		public void VisitTagReferenceField(string name, int offset, bool withGroup)
+		{
+			SeekReader(offset);
+			DatumIndex index;
+			if(withGroup)
+            {
+				var tagRefLayout = _buildInfo.Layouts.GetLayout("tag reference");
+				var tagRefValues = ReadStructure(_reader, _cache, _buildInfo, tagRefLayout);
+				index = new DatumIndex(tagRefValues.GetInteger("datum index"));
+            }
+			else
+            {
+				index = new DatumIndex(_reader.ReadUInt32());
+				_offset += 4;
+			}
+			
+			if(index.IsValid)
+            {
+				ITag tag = _cache.Tags[index];
+				_collection.SetTagReference(name, tag);
+            }
+			else
+            {
+				_collection.SetTagReference(name, null);
+            }
+		}
+
+		/// <summary>
+		///     Reads a structure from a stream by following a predefined structure layout.
+		/// </summary>
+		/// <param name="reader">The IReader to read the structure from.</param>
+		/// <param name="cache">The cache file to read from.</param>
+		/// <param name="buildInfo"></param>
+		/// <param name="layout">The structure layout to follow.</param>
+		/// <returns>A collection of the values that were read.</returns>
+		/// <seealso cref="StructureLayout" />
+		public static StructureValueCollection ReadStructure(IReader reader, ICacheFile cache, EngineDescription buildInfo, StructureLayout layout)
+		{
+			var structReader = new CacheStructureReader(reader, cache, buildInfo);
+			layout.Accept(structReader);
+			if (layout.Size > 0)
+				structReader.SeekReader(layout.Size);
+
+			return structReader._collection;
+		}
+	}
+}
