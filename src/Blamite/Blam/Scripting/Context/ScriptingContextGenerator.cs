9b31726360229d5e027e6fd91176016abbdf852e
SnipeStyle <snipestylev2@gmail.com>
2020-10-07 01:28:31 +0200

    Another attempt at fixing the context generator...
    The scripting context generator was still causing problems with certain maps. This should hopefully fix these problems.

--- src/Blamite/Blam/Scripting/Context/ScriptingContextGenerator.cs
+++ src/Blamite/Blam/Scripting/Context/ScriptingContextGenerator.cs
@@ -1,16 +1,21 @@
-﻿using Blamite.Blam.Resources.Sounds;
-using Blamite.IO;
+﻿using Blamite.IO;
 using Blamite.Serialization;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 
 namespace Blamite.Blam.Scripting.Context
 {
     public static class ScriptingContextGenerator
     {
+        /// <summary>
+        /// Generates the scripting context for a cache file.
+        /// </summary>
+        /// <param name="reader">The stream to read from.</param>
+        /// <param name="cache">The cache filr.</param>
+        /// <param name="buildinfo">The cache file's build information.</param>
+        /// <returns>Returns a <see cref="ScriptingContextCollection"/>.</returns>
         public static ScriptingContextCollection GenerateContext(IReader reader, ICacheFile cache, EngineDescription buildinfo)
         {
             var result = new ScriptingContextCollection();
@@ -36,9 +41,18 @@ namespace Blamite.Blam.Scripting.Context
             return result;
         }
 
-        private static void FillContextCollection(IReader reader, ICacheFile cache, EngineDescription buildinfo, StructureValueCollection values, ScriptingContextCollection collection)
+
+        /// <summary>
+        /// Fills a context collection with unit seat mappings and context objects.
+        /// </summary>
+        /// <param name="reader">The stream to read from.</param>
+        /// <param name="cache">The cache file containing the scripting context.</param>
+        /// <param name="buildinfo">The cache file's build information.</param>
+        /// <param name="data">The meta data in which is the context is stored.</param>
+        /// <param name="collection">The context collection.</param>
+        private static void FillContextCollection(IReader reader, ICacheFile cache, EngineDescription buildinfo, StructureValueCollection data, ScriptingContextCollection collection)
         {
-            var tagBlocks = values.GetTagBlocks();
+            var tagBlocks = data.GetTagBlocks();
 
             foreach(var block in tagBlocks)
             {
@@ -62,11 +76,24 @@ namespace Blamite.Blam.Scripting.Context
             }
         }
 
+
+        /// <summary>
+        /// Determines whether a block is a wrapper block or not.
+        /// </summary>
+        /// <param name="block">The block to examine.</param>
+        /// <returns>Returns true if the block is a wrapper block.</returns>
         private static bool IsWrapperBlock(KeyValuePair<string, StructureValueCollection[]> block)
         {
             return block.Value[0].HasStringID("Name") || block.Value[0].HasString("Name") ? false : true;
         }
 
+
+        /// <summary>
+        /// Processes a wrapper block and adds its child blocks to a context collection.
+        /// </summary>
+        /// <param name="block">The wrapper block.</param>
+        /// <param name="collection">The context collection.</param>
+        /// <exception cref="ArgumentException">Thrown when a wrapper block has more than one element.</exception>
         private static void HandleWrapperBlock(KeyValuePair<string, StructureValueCollection[]> block, ScriptingContextCollection collection)
         {
             if (block.Value.Length != 1)
@@ -82,18 +109,34 @@ namespace Blamite.Blam.Scripting.Context
             }
         }
 
-        private static ScriptingContextBlock CreateContextBlock(KeyValuePair<string, StructureValueCollection[]> block, bool isChildBlock)
+
+        /// <summary>
+        /// Creates a context block from meta data.
+        /// </summary>
+        /// <param name="data">The meta data.</param>
+        /// <param name="isChildBlock">Is true, if this block is the child of a context object.</param>
+        /// <returns>A context block.</returns>
+        private static ScriptingContextBlock CreateContextBlock(KeyValuePair<string, StructureValueCollection[]> data, bool isChildBlock)
         {
-            IEnumerable<ScriptingContextObject> objects = block.Value.Select((values, index) => CreateContextObject(index, block.Key, isChildBlock, values));
-            return new ScriptingContextBlock(block.Key, objects);
+            IEnumerable<ScriptingContextObject> objects = data.Value.Select((values, index) => CreateContextObject(index, data.Key, isChildBlock, values));
+            return new ScriptingContextBlock(data.Key, objects);
         }
 
-        private static ScriptingContextObject CreateContextObject(int index, string group, bool isChildObject, StructureValueCollection values)
+
+        /// <summary>
+        /// Creates a context object from meta data.
+        /// </summary>
+        /// <param name="index">The index of the object.</param>
+        /// <param name="group">The object group of the object.</param>
+        /// <param name="isChildObject">is true if thi sobject is the child of another object.</param>
+        /// <param name="data">The meta data.</param>
+        /// <returns>A context object.</returns>
+        private static ScriptingContextObject CreateContextObject(int index, string group, bool isChildObject, StructureValueCollection data)
         {
-            string name = values.HasStringID("Name") ? values.GetStringID("Name") : values.GetString("Name");
+            string name = data.HasStringID("Name") ? data.GetStringID("Name") : data.GetString("Name");
             var result = new ScriptingContextObject(name.ToLowerInvariant(), index, group, isChildObject);
 
-            var childBlocks = values.GetTagBlocks();
+            var childBlocks = data.GetTagBlocks();
             if(childBlocks.Length > 0)
             {
                 foreach(var block in childBlocks)
@@ -105,6 +148,15 @@ namespace Blamite.Blam.Scripting.Context
             return result;
         }
 
+
+        /// <summary>
+        /// Collects all unit seat mappings from a cache file and adds them to a context collection.
+        /// </summary>
+        /// <param name="reader">The stream to read from.</param>
+        /// <param name="cache">The cache file.</param>
+        /// <param name="buildInfo">The build information for this cache file.</param>
+        /// <param name="block">The cache file's unit seat mappings block.</param>
+        /// <param name="collection">The context collection.</param>
         private static void HandleUnitSeatMappings(IReader reader, ICacheFile cache, EngineDescription buildInfo, 
             KeyValuePair<string, StructureValueCollection[]> block, ScriptingContextCollection collection)
         {
@@ -140,7 +192,7 @@ namespace Blamite.Blam.Scripting.Context
             }
 
             // Identify and create seat mapping groups.
-            var mappings = GroupMappingNames(information);
+            var mappings = CreateUnitSeatMappings(information);
 
             // Add the final unit seat mapping objects to the result.
             foreach(var mapping in mappings)
@@ -149,54 +201,97 @@ namespace Blamite.Blam.Scripting.Context
             }
         }
 
-        private static IEnumerable<UnitSeatMapping> GroupMappingNames(MappingInformation[] information)
+
+        /// <summary>
+        /// Creates unit seat mappings from mapping information. Also identifies and handles mapping groups.
+        /// </summary>
+        /// <param name="information">The information</param>
+        /// <returns>A <see cref="IEnumerable"/> containing the finalized seat mappings.</returns>
+        private static IEnumerable<UnitSeatMapping> CreateUnitSeatMappings(MappingInformation[] information)
         {
             var queue = new Queue<MappingInformation>(information);
             var result = new List<UnitSeatMapping>();
             while (queue.Count > 0)
             {
                 MappingInformation info = queue.Dequeue();
-                short index = info.Index;
-                string name = info.Name;
-                short count = 1;
 
-                var processedInformation = new List<MappingInformation>();
+                var members = new List<MappingInformation>();
                 var processedTags = new List<string>();
-                processedInformation.Add(info);
+                members.Add(info);
                 processedTags.Add(info.TagName);
+
+                // Retrieve other group members.
                 while (queue.Count > 0)
                 {
                     MappingInformation next = queue.Peek();
-                    if(next.TagName == info.TagName || next.SplitName[0] != info.SplitName[0] || next.SplitName[1] != info.SplitName[1] 
-                        || processedInformation.Contains(next) || processedTags.Contains(next.TagName))
+
+                    // The border of a group is reached, when any of these conditions is true.
+                    if (next.TagName == info.TagName || next.SplitName[0] != info.SplitName[0] || next.SplitName[1] != info.SplitName[1] 
+                        || members.Contains(next) || processedTags.Contains(next.TagName))
                     {
                         break;
                     }
                     else
                     {
                         processedTags.Add(next.TagName);
-                        processedInformation.Add(queue.Dequeue());
+                        members.Add(queue.Dequeue());
                     }
                 }
 
-                if(processedInformation.Count > 1)
+                // Handle groups.
+                if(members.Count > 1)
                 {
-                    count = (short)processedInformation.Count;
-                    name = ShortenMappingName(processedInformation);
-                }
+                    // Some groups are not being detected correctly and need to be postprocessed. 
+                    // If subsequent group member share the same name, they belong to a separate group. In this case the original group needs to be split.
+                    List<MappingInformation> currentGroup = new List<MappingInformation> { members.First() };
+                    foreach(var i in members.Skip(1))
+                    {
+                        if(currentGroup.Count > 1 && currentGroup.Last().Name != i.Name && currentGroup.All(i => i.Name == currentGroup[0].Name))
+                        {
+                            var group = new UnitSeatMapping(currentGroup.First().Index, (short)currentGroup.Count, currentGroup.First().Name);
+                            result.Add(group);
+                            currentGroup.Clear();
+                        }
 
-                result.Add(new UnitSeatMapping(index, count, name));
+                        currentGroup.Add(i);
+                    }
+
+                    // Add the last group.
+                    var mapping = new UnitSeatMapping(currentGroup.First().Index, (short)currentGroup.Count, GetMappingGroupName(currentGroup));
+                    result.Add(mapping);
+                    currentGroup.Clear();
+                }
+                // Handle single mappings.
+                else
+                {
+                    result.Add(new UnitSeatMapping(members[0].Index, 1, members[0].Name));
+                }
             }
 
             return result;
         }
 
-        private static string ShortenMappingName(IEnumerable<MappingInformation> mappings)
+
+        /// <summary>
+        /// Gets the name for a unit seat mapping group.
+        /// </summary>
+        /// <param name="mappings">The mappings that are members of this group.</param>
+        /// <returns>The name of the group.</returns>
+        /// <exception cref="ArgumentException">Thrown when one or more mapping names have less than 2 substrings.</exception>
+        private static string GetMappingGroupName(IEnumerable<MappingInformation> mappings)
         {
             var split = mappings.Select(m => m.SplitName).ToArray();
             int maxLength = split.Min(s => s.Length);
+
+            // Mapping names consist of at least 2 substrings.
+            if(maxLength < 2)
+            {
+                throw new ArgumentException("Failed to calculate the the name for a unit seat mapping group. One or more mapping names were too short.");
+            }
+
             int matchingParts = 2;
 
+            // Calculate the longest common prefix.
             for (int i = 2; i < maxLength; i++)
             {
                 var nextPart = split.Select(s => s[i]).ToArray();
@@ -213,6 +308,13 @@ namespace Blamite.Blam.Scripting.Context
             return string.Join("_", split[0].Take(matchingParts));
         }
 
+
+        /// <summary>
+        /// Converts a unit seat mapping's two seat bitmasks to a list of all seat indices, which belong to the mapping.
+        /// </summary>
+        /// <param name="seats1">The Seats1 bitmask of the unit seat mapping.</param>
+        /// <param name="seats2">The Seats2 bitmask of the unit seat mapping.</param>
+        /// <returns>A list of all seat indices, which belong to the unit seat mapping.</returns>
         private static List<int> GetSeatIndices(int seats1, int seats2)
         {
             var bits = new BitArray(new int[] { seats1, seats2 });
@@ -229,6 +331,13 @@ namespace Blamite.Blam.Scripting.Context
             return result;
         }
 
+
+        /// <summary>
+        /// Gets the name of a single seat mapping based on the vehicle meta data.
+        /// </summary>
+        /// <param name="seats">The seats meta data of the corresponding vehi tag.</param>
+        /// <param name="indeces">The indeces of the seats, which belong to this seat mapping.</param>
+        /// <returns></returns>
         private static string GetVehicleMappingName(StructureValueCollection[] seats, IEnumerable<int> indeces)
         {
             List<string> names = new List<string>();
@@ -244,6 +353,7 @@ namespace Blamite.Blam.Scripting.Context
             var result = names.Min().TakeWhile((c, i) => names.All(s => s[i] == c)).ToArray();
             return new string(result).TrimEnd('_', '0');
         }
+
 
         private class MappingInformation
         {
